ffdf7962bae1bd925aeb45499acf70cebe7b26c2
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-04 18:39:18 -0400

    create blogs table

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from blog.routes import blog_router
+from blog import models
+from database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
