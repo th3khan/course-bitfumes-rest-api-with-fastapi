c7f4d4f0980eec04d732b2d8b38c1b9e192b10b5
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-04 18:39:07 -0400

    load .env file

--- database.py
+++ database.py
@@ -3,6 +3,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 username = os.getenv('DB_USERNAME')
 password = os.getenv('DB_PASSWORD')
