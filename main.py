6f5ab8837c6be5773a13856043155df6bb9496a8
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-04 16:49:41 -0400

    create function with both parameter types (Query and Path)

--- main.py
+++ main.py
@@ -19,3 +19,9 @@ def get_blog(limit: int, publised: bool = True, sort: Optional[str] = None):
 @app.get('/blog/{id}')
 def get_blog(id: int):
     return { 'data' : id }
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit: int):
+    return {
+        'data': f'Get {limit} comment from blog with id: {id}'
+    }
