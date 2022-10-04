6cda69fdf495ec6522fd3caf00b596c17a58c602
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-04 16:31:25 -0400

    set query parameter

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def index():
         'api': 'v1'
     }
 
+@app.get('/blog')
+def get_blog(limit: int):
+    return { 'data': 'blog list' }
+
 @app.get('/blog/{id}')
 def get_blog(id: int):
     return { 'data' : id }
