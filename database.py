2b8dfb1cdf728d1fa0459731dd70fc5fb9ce19cf
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-04 18:18:13 -0400

    create session database and set declarative base class

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 # bitfumes_tutorial_fastapi
 import os
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 username = os.getenv('DB_USERNAME')
 password = os.getenv('DB_PASSWORD')
@@ -8,8 +10,10 @@ host = os.getenv('DB_HOST')
 port = os.getenv('DB_PORT')
 database = os.getenv('DB_DATABASE')
 
-engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}')
+engine = create_engine(
+    f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
+)
 
-meta = MetaData()
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-conn = engine.connect()
+Base = declarative_base()
