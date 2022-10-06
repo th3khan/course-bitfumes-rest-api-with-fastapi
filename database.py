ede3c304b69361ba94e1c51d9aef839b59058231
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-06 00:01:55 -0400

    get_database function of Session type

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,3 +20,10 @@ engine = create_engine(
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
