10e7dc37420755d9b5925a4a4aec7189181a667b
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-07 01:08:46 -0400

    move password attribute to UserRequest model only

--- user/schemas.py
+++ user/schemas.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 class UserBase(BaseModel):
 
     email: str
-    password: str
     name: str
 
 class UserResponse(UserBase):
@@ -13,4 +12,4 @@ class UserResponse(UserBase):
         orm_mode = True
 
 class UserRequest(UserBase):
-    pass
+    password: str
