e10ee7b45f584bcd557e4bbd05042aed21a25566
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-07 01:03:10 -0400

    create helpers hashing file

--- user/routes.py
+++ user/routes.py
@@ -3,15 +3,14 @@ from unicodedata import name
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-from passlib.context import CryptContext
 
 from .schemas import UserRequest, UserResponse
 from .models import User
 from database  import get_db
+from helpers.hashing import Hash
 
 user_router = APIRouter(prefix='/users', tags=['Users'])
 
-pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @user_router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserResponse)
 def create_user(request: UserRequest, db: Session = Depends(get_db)):
@@ -20,8 +19,8 @@ def create_user(request: UserRequest, db: Session = Depends(get_db)):
     if userExists:
         raise HTTPException(detail="Email user already is registered.", status_code=status.HTTP_400_BAD_REQUEST)
 
-    hashPassword = pwd_ctx.hash(request.password)
-    new_user = User(name=request.name, email=request.email, password=hashPassword)
+    
+    new_user = User(name=request.name, email=request.email, password=Hash.bcrypy(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
