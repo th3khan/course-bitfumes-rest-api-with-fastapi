3895e35771b4e9c1d3ff7aa5d85d59cb0e403558
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-07 00:36:26 -0400

    include user routes to app

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from blog.routes import blog_router
-from blog import models
-from database import engine
+from user.routes import user_router
+from database import engine, Base
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(blog_router)
+app.include_router(user_router)
 
 @app.get('/')
 def index():
