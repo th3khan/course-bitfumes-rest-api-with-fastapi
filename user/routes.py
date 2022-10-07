454b4c92e522e130fa66c10204401c5aa5aac96b
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-07 00:57:27 -0400

    hash password

--- user/routes.py
+++ user/routes.py
@@ -3,12 +3,15 @@ from unicodedata import name
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
+from passlib.context import CryptContext
 
 from .schemas import UserRequest, UserResponse
 from .models import User
 from database  import get_db
 
 user_router = APIRouter(prefix='/users', tags=['Users'])
+
+pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @user_router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserResponse)
 def create_user(request: UserRequest, db: Session = Depends(get_db)):
@@ -17,7 +20,8 @@ def create_user(request: UserRequest, db: Session = Depends(get_db)):
     if userExists:
         raise HTTPException(detail="Email user already is registered.", status_code=status.HTTP_400_BAD_REQUEST)
 
-    new_user = User(name=request.name, email=request.email, password=request.password)
+    hashPassword = pwd_ctx.hash(request.password)
+    new_user = User(name=request.name, email=request.email, password=hashPassword)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
